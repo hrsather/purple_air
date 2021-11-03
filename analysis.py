20df87f52ba2f98e8d93523d9429adbc6b0202ac
hrsatherMINES <hrsather@mines.edu>
2021-11-03 00:22:10 -0600

    Correlation based colors

--- analysis.py
+++ analysis.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 
-def get_rmse(data, name_a, name_b, date_name, ppm_name):
-    df_a = data[data["location"] == name_a]
-    df_b = data[data["location"] == name_b]
+def get_rmse(data, location_a, location_b, date_name, ppm_name):
+    df_a = data[data["location"] == location_a]
+    df_b = data[data["location"] == location_b]
 
     # Drop na and zero ppm values
     merged_df = df_a.merge(df_b, on=[date_name])
@@ -16,3 +16,5 @@ def get_rmse(data, name_a, name_b, date_name, ppm_name):
     merged_df["squared_error"] = np.square(merged_df[ppm_x_name] - merged_df[ppm_y_name])
 
     return np.sqrt(merged_df["squared_error"].mean())
+
+
